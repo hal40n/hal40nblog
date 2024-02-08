22d6dc8b32a43ed1079caba6a62a1f79d97ec6f6
Shoma Takeuchi <takeuchi@keiyo.biz>
2024-02-08 15:03:15 +0900

    [add] トップページの追加、テーブル作成とダミーデータの追加

---
+++ database/migrations/2024_02_07_155228_create_articles_table.php
@@ -0,0 +1,51 @@
+<?php
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+class CreateArticlesTable extends Migration
+{
+
+    /**
+     * Run the migrations.
+     *
+     * @return void
+     */
+    public function up()
+    {
+        Schema::create('articles', function (Blueprint $table) {
+            $table->bigIncrements('id');
+            $table->string('title')->nullable(false);
+            $table->text('body')->nullable(false);
+            $table->bigInteger('user_id')->unsigned();
+            $table->foreign('user_id')
+                ->references('id')
+                ->on('users')
+                ->onDelete('cascade');
+            $table->bigInteger('tag_id')
+                ->unsigned()
+                ->nullable();
+            $table->foreign('tag_id')
+                ->references('id')
+                ->on('tags')
+                ->onDelete('cascade');
+            $table->timestamps();
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     *
+     * @return void
+     */
+    public function down()
+    {
+        Schema::table('articles', function (Blueprint $table) {
+            $table->dropForeign([
+                'tag_id'
+            ]);
+        });
+
+        Schema::dropIfExists('articles');
+    }
+}
--- database/seeders/DatabaseSeeder.php
+++ database/seeders/DatabaseSeeder.php
@@ -3,6 +3,9 @@
 namespace Database\Seeders;
 
 use Illuminate\Database\Seeder;
+use App\Models\Article;
+use App\Models\Tag;
+use App\Models\User;
 
 class DatabaseSeeder extends Seeder
 {
@@ -13,6 +16,19 @@ class DatabaseSeeder extends Seeder
      */
     public function run()
     {
-        // \App\Models\User::factory(10)->create();
+        // ユーザーを生成
+        $users = User::factory(100)->create();
+        
+        // タグを生成
+        $tags = Tag::factory(20)->create();
+        
+        // ユーザーごとに記事を生成し、各記事にランダムな数のタグをアタッチする
+        foreach ($users as $user) {
+            Article::factory(20)->create([
+                'user_id' => $user->id,
+            ])->each(function ($article) use ($tags) {
+                $article->tags()->attach($tags->random(random_int(1, 5)));
+            });
+        }
     }
 }
